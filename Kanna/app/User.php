00fb38f4ab05654d8a63daa8932960dfbb62bfad
Johanna Rod <39653403+yohannarr@users.noreply.github.com>
2018-05-30 18:40:58 -0400

    Prueba

--- Kanna/app/User.php
+++ Kanna/app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password33333',
+        'name', 'email', 'password4444',
     ];
 
     /**
