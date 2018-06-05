1eff82dd8bc7e2f251a65f600a076a1a864235af
KerXD <karvinjimenezxd@hotmail.com>
2018-06-04 23:15:35 -0400

    Problema del login resuelto
    Y este era el grandioso error

--- Shuxhy/app/User.php
+++ Shuxhy/app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password4444',
+        'name', 'email', 'password',
     ];
 
     /**
