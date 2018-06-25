44e3cc9ccc7d54f75b8041516f63a9e2c3f8464a
KerXD <karvinjimenezxd@hotmail.com>
2018-06-25 04:01:02 -0400

    Nuevo link
    Ahora se puede acceder a registro desde almacen

--- Shuxhy/resources/views/layouts/admin.blade.php
+++ Shuxhy/resources/views/layouts/admin.blade.php
@@ -159,7 +159,7 @@
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="registro"><i class="fa fa-circle-o"></i>Registrar</a></li>
+                <li><a href="{{ url('/register') }}"><i class="fa fa-circle-o"></i>Registrar</a></li>
                 </li>
                 
               </ul>
